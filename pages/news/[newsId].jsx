b49e811b4c7b1a3b91ff778439b17172eb7cd31f
Matt658041 <91630948+Matt658041@users.noreply.github.com>
2022-08-02 09:36:13 -0500

    learning about data fetching

--- pages/news/[newsId].jsx
+++ pages/news/[newsId].jsx
@@ -1,9 +1,15 @@
-
+import {useRouter} from 'next/router'
 
 
 function DetailPage () {
+    const router = useRouter();
+
+    const newsId = router.query.newsId;
+
+     // this is where you could send a request to backend api
+     //or fetch the news items with newsId
+
     return <h1>The Detail Page</h1>
-    
     }
     
     export default DetailPage;
